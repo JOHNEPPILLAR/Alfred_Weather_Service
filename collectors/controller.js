88188088fd40cffe90fbfcaafa7379b0b6cd1835
JP <john.pillar@rbs.co.uk>
2019-03-12 22:38:55 +0000

    added mock mode

--- collectors/controller.js
+++ collectors/controller.js
@@ -2,12 +2,15 @@
  * Import helper libraries
  */
 const serviceHelper = require('../lib/helper.js');
-const pureCool = require('./dyson/purecool.js');
 
-exports.collectData = async function FnCollectData() {
-  try {
-    await pureCool.processPureCoolData(); // Collect Dyson Pure Cool device data
-  } catch (err) {
-    serviceHelper.log('error', 'Controller - CollectData', err.message);
-  }
-};
+if (!process.env.Mock) {
+  // eslint-disable-next-line global-require
+  const pureCool = require('./dyson/purecool.js');
+  exports.collectData = async function FnCollectData() {
+    try {
+      await pureCool.processPureCoolData(); // Collect Dyson Pure Cool device data
+    } catch (err) {
+      serviceHelper.log('error', 'Controller - CollectData', err.message);
+    }
+  };
+}
