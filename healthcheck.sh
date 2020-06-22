ecd75e85ff51e9ae6afc4ed61c61fb734a3ab8df
JOHNEPPILLAR <johneppillar@gmail.com>
2020-06-22 20:08:13 +0100

    Update to use new routing pattern

--- healthcheck.sh
+++ healthcheck.sh
@@ -5,6 +5,7 @@ PORT=3978
 SERVICE_NAME=$(node -p -e "require('./package.json').name")
 URL=https://$SERVICE_NAME:$PORT/ping
 
+echo "Call url"
 if [ $(curl -L --insecure $URL -o /dev/null -w '%{http_code}\n' -s) == "401" ]
 then exit 0
 else exit 1
